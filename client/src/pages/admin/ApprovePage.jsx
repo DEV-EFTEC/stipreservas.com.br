3b38189da9b0049a992e7f4858dcbaf9f9b360ec
Dev-Lyh <devlyh.testing@gmail.com>
2025-06-09 19:14:37 -0300

    feat:init approve page

---
+++ client/src/pages/admin/ApprovePage.jsx
@@ -0,0 +1,12 @@
+import GlobalBreadcrumb from "@/components/associate/GlobalBreadcrumb";
+
+export default function ApprovePage() {
+  return (
+    <section className="flex w-full p-20 justify-between">
+      <GlobalBreadcrumb />
+      <div className="flex gap-12 items-end mb-8">
+        <Text></Text>
+      </div>
+    </section>
+  )
+}
