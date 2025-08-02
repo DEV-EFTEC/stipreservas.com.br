7e881797788a691d74ffba78c12655d96c8271c3
Dev-Lyh <devlyh.testing@gmail.com>
2025-08-02 16:00:24 -0300

    feat:create new superuser route

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,15 +1,20 @@
-import express from 'express';
-import * as userController from '../controllers/userController.js';
+import express from "express";
+import * as userController from "../controllers/userController.js";
 import auth from "#middlewares/auth.js";
 
 const router = express.Router();
 
-router.post('/register', userController.register);
-router.post('/find-user-by-id', userController.findUserById);
-router.patch('/update-user/:id', userController.updateUser);
-router.post('/login', userController.signIn);
-router.post('/find-user-by-cpf', userController.findUserByCpf);
-router.post('/generate-registration-link', auth, userController.registrationLink);
-router.post('/verify-registration-link', userController.verifyToken);
+router.post("/register", userController.register);
+router.post("/find-user-by-id", userController.findUserById);
+router.patch("/update-user/:id", userController.updateUser);
+router.post("/login", userController.signIn);
+router.post("/find-user-by-cpf", userController.findUserByCpf);
+router.post(
+  "/generate-registration-link",
+  auth,
+  userController.registrationLink
+);
+router.post("/create-new-superuser", auth, userController.createNewSuperuser);
+router.post("/verify-registration-link", userController.verifyToken);
 
 export default router;
