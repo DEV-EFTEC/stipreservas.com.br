1687067ced0a25894a3ab794c7575095b9b675f5
Dev-Lyh <devlyh.testing@gmail.com>
2025-08-25 08:43:07 -0300

    refactor:remove Boom of ListDependents

--- client/src/pages/associate/Escorts/ListDependents.jsx
+++ client/src/pages/associate/Escorts/ListDependents.jsx
@@ -23,10 +23,6 @@ import validarCpf from "validar-cpf";
 import { useDynamicList } from "@/hooks/useDynamicList";
 import { toast } from "sonner";
 
-function Boom() {
-  throw new Error("Boom!");
-}
-
 export default function ListDependents() {
   const { user } = useAuth();
 
@@ -66,7 +62,6 @@ export default function ListDependents() {
 
   return (
     <section className="w-full xl:p-20 pr-2 overflow-y-auto">
-      <Boom />
       {
         dependents
         &&
