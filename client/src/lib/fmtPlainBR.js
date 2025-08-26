61a0884d99060e1151aa11c9fc9c26a7d34ff7dc
Dev-Lyh <devlyh.testing@gmail.com>
2025-08-26 12:55:58 -0300

    fix:finish book date

--- client/src/lib/fmtPlainBR.js
+++ client/src/lib/fmtPlainBR.js
@@ -2,7 +2,7 @@ import { parse, format, isDate } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export const fmtPlainBR = (s) =>
-  format(parse(s, "yyyy-MM-dd", new Date()), "d 'de' MMMM (eee)", {
+  format(parse(s.includes("T") ? s.split("T")[0] : s, "yyyy-MM-dd", new Date()), "d 'de' MMMM (eee)", {
     locale: ptBR,
   });
 
